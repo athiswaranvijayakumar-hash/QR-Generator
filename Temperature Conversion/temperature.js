cd4621471e09fd70d964716b174afc8b17473cdd
athiswaranvijayakumar-hash <athiswaranvijayakumar@gmail.com>
2026-02-01 17:10:27 +0530

    small changes in js file

--- Temperature Conversion/temperature.js
+++ Temperature Conversion/temperature.js
@@ -4,11 +4,11 @@ var input =Number(document.getElementById("numberbox").value);
 var output = document.getElementById("output");
 if (choice === "fahrenheit"){
   answer = (9/5) * input + 32;
-  
+  answer = answer.toFixed(2);
 }
 else{
   answer = (input-32)/1.8;
- 
+ answer = answer.toFixed(2);
 }
 output.style.display = "block";
  output.textContent = answer;
